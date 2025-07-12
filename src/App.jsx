88bbfe6e771d4642b92eb8427fc16063de796a8e
Roman Zaichenko <romanzaichenko33@gmail.com>
2025-07-12 14:23:24 +0300

    Create logic for adding item to the list.

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, {useState} from "react";
+import "bootstrap/dist/css/bootstrap.css"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Button from "react-bootstrap/Button"
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import ListGroup from "react-bootstrap/ListGroup"
+
+
+function App() {
+  const [input, setInput] = useState('')
+  const [list, setList] = useState([])
+
+  const addItem = (item) => {
+    if (input !== "") {
+      const userInput ={
+        id: Math.random(),
+        value: input
+      }
+
+      const updatedList = [...list];
+      updatedList.push(userInput)
+
+      setList(updatedList)
+      setInput("")
+    }
+  }
+}
